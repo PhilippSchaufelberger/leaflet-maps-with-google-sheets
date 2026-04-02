580b52d62240e92fe96dd4baa87566dd83834350
PhilippSchaufelberger <geschichtekfr@gmail.com>
2026-04-02 20:52:20 +0200

    Update google-doc-url.js

--- google-doc-url.js
+++ google-doc-url.js
@@ -1,5 +1,5 @@
 // paste in your published Google Sheets URL from the browser address bar
-var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vQt66bPte1V8vvsiSF8CyRQEmpyDdVG9P5reLtQjUsMVPPcpdcXYjuj9wHWdpON3-OUFA_EygIkqYzz/pub?output=csv';
+var googleDocURL = 'https://docs.google.com/spreadsheets/d/e/2PACX-1vTnQ0vqXBiisBQBqGb7UWnIOCiaSoyFwqE2ZR8tnbOfNb7IlCrHYDao6j4XK5iE9656pPceEBtrC64w/pub?output=csv';
 
 // insert your own Google Sheets API key from https://console.developers.google.com
 var googleApiKey = 'AIzaSyBh9nKnVZm2RPeZa0ywCOxPAgJJfK87WhY';
